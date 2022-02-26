37a7acad05ebb36caa9b3e6c8cb6a9bfe71ce5fc
hus197 <94990126+hus197@users.noreply.github.com>
2022-02-26 22:40:53 +0000

    Add files via upload

---
+++ HLTWK11Ahmad.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+mpg
+
+#displ on the x-axis and hwy on the y-axis:
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+#adding colour to data points
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color = class))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
+
+
+#chaning size of data points
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, size = class))
+
+#aplha changes transparency of points
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+
+#changin data points shape
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, shape = class))
+
+
+
+
+#bar chart
+ggplot(data=mpg, aes(x=manufacturer, y=cty)) +
+  geom_bar(stat="identity", width=0.5)
+
+# Change colors
+ggplot(data=mpg, aes(x=manufacturer, y=cty)) +
+  geom_bar(stat="identity", color="blue", fill="white")
+
+# Minimal theme + blue fill color
+ggplot(data=mpg, aes(x=manufacturer, y=cty)) +
+geom_bar(stat="identity", fill="steelblue")+
+theme_minimal()
